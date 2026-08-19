00955100c813be6a213b80ccd45b3a448f43dd8d
Mustafa <mustafa14oner@gmail.com>
2026-08-19 20:22:46 +0300

    Yeni fotoğraflar ve kategoriler eklendi

--- gallery-data.js
+++ gallery-data.js
@@ -56,6 +56,211 @@ window.GALLERY_IMAGES = [
         "path":  "images/doga/martı_günbatımıdeniz.JPG"
     },
     {
+        "name":  "IMG_3734.JPG",
+        "category":  "gokceeada",
+        "path":  "images/gokceeada/IMG_3734.JPG"
+    },
+    {
+        "name":  "IMG_3739.JPG",
+        "category":  "gokceeada",
+        "path":  "images/gokceeada/IMG_3739.JPG"
+    },
+    {
+        "name":  "IMG_3740.JPG",
+        "category":  "gokceeada",
+        "path":  "images/gokceeada/IMG_3740.JPG"
+    },
+    {
+        "name":  "IMG_3743.JPG",
+        "category":  "gokceeada",
+        "path":  "images/gokceeada/IMG_3743.JPG"
+    },
+    {
+        "name":  "IMG_3745.JPG",
+        "category":  "gokceeada",
+        "path":  "images/gokceeada/IMG_3745.JPG"
+    },
+    {
+        "name":  "IMG_3746.JPG",
+        "category":  "gokceeada",
+        "path":  "images/gokceeada/IMG_3746.JPG"
+    },
+    {
+        "name":  "IMG_3748.JPG",
+        "category":  "gokceeada",
+        "path":  "images/gokceeada/IMG_3748.JPG"
+    },
+    {
+        "name":  "IMG_3749.JPG",
+        "category":  "gokceeada",
+        "path":  "images/gokceeada/IMG_3749.JPG"
+    },
+    {
+        "name":  "IMG_3751.JPG",
+        "category":  "gokceeada",
+        "path":  "images/gokceeada/IMG_3751.JPG"
+    },
+    {
+        "name":  "IMG_3752.JPG",
+        "category":  "gokceeada",
+        "path":  "images/gokceeada/IMG_3752.JPG"
+    },
+    {
+        "name":  "IMG_3755.JPG",
+        "category":  "gokceeada",
+        "path":  "images/gokceeada/IMG_3755.JPG"
+    },
+    {
+        "name":  "IMG_3756.JPG",
+        "category":  "gokceeada",
+        "path":  "images/gokceeada/IMG_3756.JPG"
+    },
+    {
+        "name":  "IMG_3759.JPG",
+        "category":  "gokceeada",
+        "path":  "images/gokceeada/IMG_3759.JPG"
+    },
+    {
+        "name":  "IMG_3760.JPG",
+        "category":  "gokceeada",
+        "path":  "images/gokceeada/IMG_3760.JPG"
+    },
+    {
+        "name":  "IMG_3761.JPG",
+        "category":  "gokceeada",
+        "path":  "images/gokceeada/IMG_3761.JPG"
+    },
+    {
+        "name":  "IMG_3762.JPG",
+        "category":  "gokceeada",
+        "path":  "images/gokceeada/IMG_3762.JPG"
+    },
+    {
+        "name":  "IMG_3763.JPG",
+        "category":  "gokceeada",
+        "path":  "images/gokceeada/IMG_3763.JPG"
+    },
+    {
+        "name":  "IMG_3764.JPG",
+        "category":  "gokceeada",
+        "path":  "images/gokceeada/IMG_3764.JPG"
+    },
+    {
+        "name":  "IMG_3767.JPG",
+        "category":  "gokceeada",
+        "path":  "images/gokceeada/IMG_3767.JPG"
+    },
+    {
+        "name":  "IMG_3768.JPG",
+        "category":  "gokceeada",
+        "path":  "images/gokceeada/IMG_3768.JPG"
+    },
+    {
+        "name":  "IMG_3770.JPG",
+        "category":  "gokceeada",
+        "path":  "images/gokceeada/IMG_3770.JPG"
+    },
+    {
+        "name":  "26_12günbatımı.JPG",
+        "category":  "gunbatımı",
+        "path":  "images/gunbatımı/26_12günbatımı.JPG"
+    },
+    {
+        "name":  "altınsu_günbatımı.jpg",
+        "category":  "gunbatımı",
+        "path":  "images/gunbatımı/altınsu_günbatımı.jpg"
+    },
+    {
+        "name":  "bitki_günbatımı.JPG",
+        "category":  "gunbatımı",
+        "path":  "images/gunbatımı/bitki_günbatımı.JPG"
+    },
+    {
+        "name":  "bulut_günbatımı.JPG",
+        "category":  "gunbatımı",
+        "path":  "images/gunbatımı/bulut_günbatımı.JPG"
+    },
+    {
+        "name":  "deniz_günbatımı.jpg",
+        "category":  "gunbatımı",
+        "path":  "images/gunbatımı/deniz_günbatımı.jpg"
+    },
+    {
+        "name":  "diken_günbatımı.jpg",
+        "category":  "gunbatımı",
+        "path":  "images/gunbatımı/diken_günbatımı.jpg"
+    },
+    {
+        "name":  "feribot_günbatımı.JPG",
+        "category":  "gunbatımı",
+        "path":  "images/gunbatımı/feribot_günbatımı.JPG"
+    },
+    {
+        "name":  "IMG_6079.JPG",
+        "category":  "gunbatımı",
+        "path":  "images/gunbatımı/IMG_6079.JPG"
+    },
+    {
+        "name":  "IMG_6122.JPG",
+        "category":  "gunbatımı",
+        "path":  "images/gunbatımı/IMG_6122.JPG"
+    },
+    {
+        "name":  "IMG_6138.JPG",
+        "category":  "gunbatımı",
+        "path":  "images/gunbatımı/IMG_6138.JPG"
+    },
+    {
+        "name":  "IMG_6146.JPG",
+        "category":  "gunbatımı",
+        "path":  "images/gunbatımı/IMG_6146.JPG"
+    },
+    {
+        "name":  "IMG_6170.JPG",
+        "category":  "gunbatımı",
+        "path":  "images/gunbatımı/IMG_6170.JPG"
+    },
+    {
+        "name":  "IMG_6200.JPG",
+        "category":  "gunbatımı",
+        "path":  "images/gunbatımı/IMG_6200.JPG"
+    },
+    {
+        "name":  "IMG_6222.JPG",
+        "category":  "gunbatımı",
+        "path":  "images/gunbatımı/IMG_6222.JPG"
+    },
+    {
+        "name":  "IMG_6231.JPG",
+        "category":  "gunbatımı",
+        "path":  "images/gunbatımı/IMG_6231.JPG"
+    },
+    {
+        "name":  "karacaören_gün batımı.JPG",
+        "category":  "gunbatımı",
+        "path":  "images/gunbatımı/karacaören_gün batımı.JPG"
+    },
+    {
+        "name":  "martı_günbatımıdeniz.JPG",
+        "category":  "gunbatımı",
+        "path":  "images/gunbatımı/martı_günbatımıdeniz.JPG"
+    },
+    {
+        "name":  "gökyüzü martilar.jpg",
+        "category":  "manzara",
+        "path":  "images/manzara/gökyüzü martilar.jpg"
+    },
+    {
+        "name":  "harabe ev _mavikadraj.mustafa.JPG",
+        "category":  "manzara",
+        "path":  "images/manzara/harabe ev _mavikadraj.mustafa.JPG"
+    },
+    {
+        "name":  "IMG_4965.JPG",
+        "category":  "manzara",
+        "path":  "images/manzara/IMG_4965.JPG"
+    },
+    {
         "name":  "IMG_5001.JPG",
         "category":  "manzara",
         "path":  "images/manzara/IMG_5001.JPG"
@@ -64,5 +269,110 @@ window.GALLERY_IMAGES = [
         "name":  "IMG_5054.JPG",
         "category":  "manzara",
         "path":  "images/manzara/IMG_5054.JPG"
+    },
+    {
+        "name":  "kaleköy3✨.JPG",
+        "category":  "manzara",
+        "path":  "images/manzara/kaleköy3✨.JPG"
+    },
+    {
+        "name":  "teknebulut.JPG",
+        "category":  "manzara",
+        "path":  "images/manzara/teknebulut.JPG"
+    },
+    {
+        "name":  "teknebulut2.JPG",
+        "category":  "manzara",
+        "path":  "images/manzara/teknebulut2.JPG"
+    },
+    {
+        "name":  "tekneDeniz.JPG",
+        "category":  "manzara",
+        "path":  "images/manzara/tekneDeniz.JPG"
+    },
+    {
+        "name":  "tepeköy4✨.JPG",
+        "category":  "manzara",
+        "path":  "images/manzara/tepeköy4✨.JPG"
+    },
+    {
+        "name":  "IMG_5905.JPG",
+        "category":  "tirilye",
+        "path":  "images/tirilye/IMG_5905.JPG"
+    },
+    {
+        "name":  "IMG_5910.JPG",
+        "category":  "tirilye",
+        "path":  "images/tirilye/IMG_5910.JPG"
+    },
+    {
+        "name":  "IMG_5919.JPG",
+        "category":  "tirilye",
+        "path":  "images/tirilye/IMG_5919.JPG"
+    },
+    {
+        "name":  "IMG_5920.JPG",
+        "category":  "tirilye",
+        "path":  "images/tirilye/IMG_5920.JPG"
+    },
+    {
+        "name":  "IMG_5925.JPG",
+        "category":  "tirilye",
+        "path":  "images/tirilye/IMG_5925.JPG"
+    },
+    {
+        "name":  "IMG_5926.JPG",
+        "category":  "tirilye",
+        "path":  "images/tirilye/IMG_5926.JPG"
+    },
+    {
+        "name":  "IMG_5930.JPG",
+        "category":  "tirilye",
+        "path":  "images/tirilye/IMG_5930.JPG"
+    },
+    {
+        "name":  "IMG_5931.JPG",
+        "category":  "tirilye",
+        "path":  "images/tirilye/IMG_5931.JPG"
+    },
+    {
+        "name":  "IMG_5932.JPG",
+        "category":  "tirilye",
+        "path":  "images/tirilye/IMG_5932.JPG"
+    },
+    {
+        "name":  "IMG_5933.JPG",
+        "category":  "tirilye",
+        "path":  "images/tirilye/IMG_5933.JPG"
+    },
+    {
+        "name":  "IMG_5934.JPG",
+        "category":  "tirilye",
+        "path":  "images/tirilye/IMG_5934.JPG"
+    },
+    {
+        "name":  "IMG_5972.JPG",
+        "category":  "tirilye",
+        "path":  "images/tirilye/IMG_5972.JPG"
+    },
+    {
+        "name":  "IMG_5973.JPG",
+        "category":  "tirilye",
+        "path":  "images/tirilye/IMG_5973.JPG"
+    },
+    {
+        "name":  "IMG_6008.JPG",
+        "category":  "tirilye",
+        "path":  "images/tirilye/IMG_6008.JPG"
+    },
+    {
+        "name":  "IMG_6015.JPG",
+        "category":  "tirilye",
+        "path":  "images/tirilye/IMG_6015.JPG"
+    },
+    {
+        "name":  "IMG_6028.JPG",
+        "category":  "tirilye",
+        "path":  "images/tirilye/IMG_6028.JPG"
     }
 ];
